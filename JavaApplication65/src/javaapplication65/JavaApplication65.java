7b3aa0ef6822676bced1871b6feed0050415f365
SujanVBharadwaj <sujanbharadwaj06@gmail.com>
2025-02-18 05:05:08 +0530

    Second commit

---
+++ JavaApplication65/src/javaapplication65/JavaApplication65.java
@@ -0,0 +1,13 @@
+package javaapplication65;
+import java.util.*;
+import java.util.Collections;
+public class JavaApplication65 {
+    public static void main(String[] args)
+    {
+        System.out.println("This is a demo program");
+        ArrayList<String>al=new ArrayList<>();
+        al.add("Sujan");
+        System.out.println("Arrylist is");
+        System.out.println(al);
+    }
+}
